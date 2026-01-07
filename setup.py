92aea5fe03a00619b68381297043bad0730e8549
Ran Aroussi <ran@aroussi.com>
2026-01-07 13:23:15 +0000

    Prepare PyPI packaging (muxi dist, manifest)

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="muxi-client",
+    name="muxi",  # package name on PyPI
     version="0.20260106.1",
-    description="MUXI Client (Python SDK)",
+    description="MUXI Python SDK",
     author="Ran Aroussi",
     author_email="ran@aroussi.com",
-    packages=find_packages(),
-    install_requires=[
-        # Add web-specific dependencies here
-    ],
+    packages=find_packages(exclude=["tests", "examples"]),
+    install_requires=[],
     python_requires=">=3.10",
+    license="Apache-2.0",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
 )
