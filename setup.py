683eba8b696382357a83c65b941cbd19197d3815
Ran Aroussi <ran@aroussi.com>
2026-01-07 13:34:20 +0000

    Modernize packaging with pyproject and dynamic version

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+try:
+    from muxi.version import __version__
+except Exception:
+    __version__ = "0.0.0"
+
+
 setup(
-    name="muxi",  # package name on PyPI
-    version="0.20260106.1",
+    name="muxi",
+    version=__version__,
     description="MUXI Python SDK",
     author="Ran Aroussi",
     author_email="ran@aroussi.com",
@@ -11,10 +17,21 @@ setup(
     python_requires=">=3.10",
     license="Apache-2.0",
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
+    url="https://github.com/muxi-ai/muxi-python",
+    project_urls={
+        "Homepage": "https://muxi.org",
+        "Source": "https://github.com/muxi-ai/muxi-python",
+        "Issues": "https://github.com/muxi-ai/muxi-python/issues",
+    },
 )
